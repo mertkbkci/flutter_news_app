da7b606683677e0ccc078cece8bf2194f560f7d9
mertkbkci <metrkbkci@gmail.com>
2023-07-31 11:37:16 +0300

    initial commit

---
+++ lib/product/constrants/image_constrants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class ImageConstrants {
+
+  const ImageConstrants._();
+  static const String microphone = "Flutter Demo";
+}
---
+++ lib/product/constrants/string_constrants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+
+@immutable
+class StringConstrants {
+
+  const StringConstrants._();
+  static const String appName = "Flutter Demo";
+}

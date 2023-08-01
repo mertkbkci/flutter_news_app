acf7bb9a80b8ecf8dcbb541f32c8ce98d9610ea2
mertkbkci <metrkbkci@gmail.com>
2023-08-01 10:34:35 +0300

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_news_app/firebase_options.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() => runApp(const ProviderScope(child: MyApp()));
+void main() {
+  FlutterError.onError = FlutterError.dumpErrorToConsole;
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const ProviderScope(child: MyApp()));
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,16 +17,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Meterial App',
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Meterial App Bar'),
+        appBar: AppBar(
+          title: const Text('Meterial App Bar'),
+        ),
+        body: const Center(
+          child: Text(
+            'Hello World',
+            style: TextStyle(fontSize: 24),
           ),
-          body: const Center(
-            child: Text(
-              'Hello World',
-              style: TextStyle(fontSize: 24),
-            ),
-          ),
-          ),
+        ),
+      ),
     );
   }
 }
